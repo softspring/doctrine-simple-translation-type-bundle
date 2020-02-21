43126f6a99ca6da6c0b63a3c83f1d19df50ea5bb
Javi H. Gil <javihgil@gmail.com>
2020-02-22 00:44:15 +0100

    Autoconfigure doctrine dbal type

--- DependencyInjection/SfsDoctrineSimpleTranslationTypeExtension.php
+++ DependencyInjection/SfsDoctrineSimpleTranslationTypeExtension.php
@@ -2,12 +2,14 @@
 
 namespace Softspring\DoctrineSimpleTranslationTypeBundle\DependencyInjection;
 
+use Softspring\DoctrineSimpleTranslationTypeBundle\Doctrine\Type\SimpleTranslationType;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\PrependExtensionInterface;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 
-class SfsDoctrineSimpleTranslationTypeExtension extends Extension
+class SfsDoctrineSimpleTranslationTypeExtension extends Extension implements PrependExtensionInterface
 {
     /**
      * @inheritdoc
@@ -17,5 +19,15 @@ class SfsDoctrineSimpleTranslationTypeExtension extends Extension
         $loader = new YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config/services'));
         $loader->load('twig_extension.yaml');
         $loader->load('type_guesser.yaml');
+    }
+
+    public function prepend(ContainerBuilder $container)
+    {
+        $doctrineConfig = [];
+
+        // add custom doctrine type
+        $doctrineConfig['dbal']['types']['simple_translation'] = SimpleTranslationType::class;
+
+        $container->prependExtensionConfig('doctrine', $doctrineConfig);
     }
 }
