a523724e448a7dbe77a43290557d84f5c243defd
Javi H. Gil <javihgil@softspring.es>
2024-01-02 12:43:04 +0100

    Fix SimpleTranslationTypeGuesser type hints

--- src/Form/SimpleTranslationTypeGuesser.php
+++ src/Form/SimpleTranslationTypeGuesser.php
@@ -7,10 +7,11 @@ use Symfony\Bridge\Doctrine\Form\DoctrineOrmTypeGuesser;
 use Symfony\Component\Form\FormTypeGuesserInterface;
 use Symfony\Component\Form\Guess\Guess;
 use Symfony\Component\Form\Guess\TypeGuess;
+use Symfony\Component\Form\Guess\ValueGuess;
 
 class SimpleTranslationTypeGuesser extends DoctrineOrmTypeGuesser implements FormTypeGuesserInterface
 {
-    public function guessType($class, $property)
+    public function guessType($class, $property): ?TypeGuess
     {
         if (!$ret = $this->getMetadata($class)) {
             return new TypeGuess('Symfony\Component\Form\Extension\Core\Type\TextType', [], Guess::LOW_CONFIDENCE);
@@ -26,17 +27,17 @@ class SimpleTranslationTypeGuesser extends DoctrineOrmTypeGuesser implements For
         return null;
     }
 
-    public function guessRequired($class, $property)
+    public function guessRequired($class, $property): ?ValueGuess
     {
         return null;
     }
 
-    public function guessMaxLength($class, $property)
+    public function guessMaxLength($class, $property): ?ValueGuess
     {
         return null;
     }
 
-    public function guessPattern($class, $property)
+    public function guessPattern($class, $property): ?ValueGuess
     {
         return null;
     }
