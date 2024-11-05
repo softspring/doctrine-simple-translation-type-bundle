276c80dd2c381d722dade7dd9442f98dd0bb5867
Javi H. Gil <javihgil@softspring.es>
2024-11-05 08:19:17 +0100

    Remove Extension component deprecation

--- src/DependencyInjection/SfsDoctrineSimpleTranslationTypeExtension.php
+++ src/DependencyInjection/SfsDoctrineSimpleTranslationTypeExtension.php
@@ -5,9 +5,9 @@ namespace Softspring\DoctrineSimpleTranslationTypeBundle\DependencyInjection;
 use Softspring\DoctrineSimpleTranslationTypeBundle\Doctrine\Type\SimpleTranslationType;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\Extension;
 use Symfony\Component\DependencyInjection\Extension\PrependExtensionInterface;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
-use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 
 class SfsDoctrineSimpleTranslationTypeExtension extends Extension implements PrependExtensionInterface
 {
